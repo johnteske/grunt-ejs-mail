106689f561e5174c1ecefe906645c5e667ce2a7e
johnteske <john.teske@gmail.com>
2016-06-29 21:42:20 -0700

    Add getData() to load project-specific YAML data

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,11 @@
 module.exports = function(grunt) {
 
+    var project = 'project';
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        // add global data
+        // company, logo, social links, etc.
         // data: grunt.file.readJSON('data.json'),
 
         ejs: {
@@ -11,6 +15,9 @@ module.exports = function(grunt) {
                     url: function(url) {
                         return 'http://example.com/' + url;
                     },
+                    getData: function(path) {
+                        return grunt.file.readYAML('src/' + project + '/' + path);
+                    }
                 },
                 cwd: 'src/',
                 src: '**/*.ejs', // '!partials/**/*'],
