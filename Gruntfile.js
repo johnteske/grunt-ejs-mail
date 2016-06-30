bbebfd8f68e286b3970ee6ae89afb6be6d8c7344
johnteske <john.teske@gmail.com>
2016-06-29 20:08:11 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // data: grunt.file.readJSON('data.json'),
+
+        ejs: {
+            all: {
+                options: {
+                    title: 'My Email',
+                    url: function(url) {
+                        return 'http://example.com/' + url;
+                    },
+                },
+                src: ['src/**/*.ejs', '!partials/**/*'],
+                dest: 'dist/',
+                expand: true,
+                ext: '.html',
+                },
+            },
+        });
+
+    grunt.loadNpmTasks('grunt-ejs');
+    grunt.registerTask('default', ['ejs']);
+
+};
