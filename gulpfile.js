89db6b86386ff7830ae60fcaee65eeedab15d4bd
johnteske <john.teske@gmail.com>
2016-07-07 22:38:04 -0700

    Initial gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    ejs = require('gulp-ejs');
+
+var project = 'project/';
+
+gulp.task('ejs', function(){
+    return gulp.src('src/' + project + '/**/*.ejs')
+        .pipe(ejs({}, {ext:'.html'}))
+        .pipe(gulp.dest('dist/' + project))
+});
+
+gulp.task('default', ['ejs']);
