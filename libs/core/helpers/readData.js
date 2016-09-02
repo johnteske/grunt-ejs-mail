677dd3b4df36196a8fa0494ed0d73cff7b326286
johnteske <john.teske@gmail.com>
2016-09-02 12:02:37 -0700

    Add readAllData() helper
    Issue #4
    
    Read all data from a given directory, returned as an object
    
    Usage:
    `<% d = core.helper.readAllData(dir.source) %>`

--- libs/core/helpers/readData.js
+++ libs/core/helpers/readData.js
@@ -2,8 +2,24 @@ var fs = require('fs'),
     path = require('path'),
     yaml = require('js-yaml');
 
-module.exports.readData = function (dataPath) {
+var readData = function(dataPath) {
     var ext = path.extname(dataPath);
     if (ext === '.json') return JSON.parse(fs.readFileSync(dataPath));
     else if (ext === '.yml') return yaml.safeLoad(fs.readFileSync(dataPath, 'utf-8'));
 };
+module.exports.readData = readData;
+
+var readAllData = function(dataPath) {
+    var datas = fs.readdirSync(dataPath),
+        dataObj = {};
+    for (var i = 0; i < datas.length; i++) {
+        var file = datas[i],
+            ext = path.extname(file),
+            base = path.basename(file).split('.')[0]; // remove extension
+        if (ext === '.json' || ext === '.yml') {
+            dataObj[base] = readData(dataPath + file);
+        }
+    }
+    return dataObj;
+};
+module.exports.readAllData = readAllData;
