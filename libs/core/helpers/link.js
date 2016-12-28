24a28694b23005ce9f12a83e5d070bb9c5843203
John Teske <jteske@john-macpro.fammain.com>
2016-12-28 11:17:03 -0800

    Allow customClass in core link helper

--- libs/core/helpers/link.js
+++ libs/core/helpers/link.js
@@ -1,10 +1,11 @@
 var url_utm = require('./url_utm.js')['url_utm'];
-module.exports.link = function (text, url, source, campaign) {
+module.exports.link = function (text, url, source, campaign, customClass) {
     source = production ? data.utm_source : 'EMAILNAME'; // redundant, matches url_utm
+    var thisClass = customClass ? customClass : '';
     if(!campaign) {
         campaign = text;
     } else if(campaign === 'null') {
         campaign = undefined; // do not track if specified
     }
-    return '<a href="' + url_utm(url, source, campaign) + '"><span class="link">' + text + '</span></a>';
+    return '<a href="' + url_utm(url, source, campaign) + '" class="' + thisClass + '"><span class="link ' + thisClass + '">' + text + '</span></a>';
 };
